28b5a2d88cce3aa8b007cbbcb65e907e73e817ae
Tony Stone <anthony.dionise@gmail.com>
2026-01-11 13:56:22 -0500

    chore: remove firebase config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,7 +23,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      youtubeApiKey: process.env.NUXT_PUBLIC_YOUTUBE_API_KEY || 'AIzaSyA8jagLA7xPLvibUtFPfHWpAXNyIK2fUMs',
+      youtubeApiKey: process.env.NUXT_PUBLIC_YOUTUBE_API_KEY ,
       youtubeEndpoint: process.env.NUXT_PUBLIC_YOUTUBE_ENDPOINT || 'https://www.googleapis.com/youtube/v3/search'
     }
   }
